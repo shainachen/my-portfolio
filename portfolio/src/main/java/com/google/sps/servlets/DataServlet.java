e6c807aac7cfcd3b95045b0becb4196d6bd96fec
Shaina Chen <shaina.sierra@gmail.com>
2020-06-03 21:33:19 +0000

    Remove comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -37,10 +37,6 @@ public class DataServlet extends HttpServlet {
   
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    /*
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Shaina!");
-    */
     String json = convertToJsonUsingGson(messages);
     response.setContentType("application/json;");
     response.getWriter().println(json);
