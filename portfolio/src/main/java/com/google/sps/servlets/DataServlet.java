9bb4d33170e1790599af78ffb2540c447f2d7dc2
Shaina Chen <shaina.sierra@gmail.com>
2020-06-05 20:21:20 +0000

    Refactor code and make minor format changes

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,34 +13,39 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
-import java.util.List;
-import java.util.Arrays;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  
+
   private List<String> messages;
 
   @Override
   public void init() {
-    messages = Arrays.asList("Wow", "Love those pictures", "Yes I am commenting on my own website");
+    messages = Arrays.asList("Wowzers", "Love those pictures", "Yes I am commenting on my own website");
   }
-  
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
-    String message = convertToJsonUsingGson(messages);
-    response.getWriter().println(message);
+    response.getWriter().println(convertToJsonUsingGson(messages));
   }
-
-   private String convertToJsonUsingGson(List messages) {
+  private String convertToJsonUsingGson(List messages) {
     Gson gson = new Gson();
     return gson.toJson(messages);
   }
