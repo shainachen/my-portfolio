c427b352cf6c33bbf8b70a452122192133b5c59c
Shaina Chen <shaina.sierra@gmail.com>
2020-06-10 16:56:05 +0000

    Add constants

--- portfolio/src/main/java/com/google/sps/Constants.java
+++ portfolio/src/main/java/com/google/sps/Constants.java
@@ -6,4 +6,7 @@ public final class Constants {
   public static final String COMMENT_ENTITY_NAME = "Comment";
   public static final String COMMENT_NAME_ID = "nameText";
   public static final String COMMENT_TEXT_ID = "commentText";
+  public static final String REQUEST_COMMENT_PARAM = "comment-input";
+  public static final String REQUEST_NAME_PARAM = "name-input";
+  public static final String REQUEST_NUMCOMMENTS_PARAM = "numberofcomments";
 }
