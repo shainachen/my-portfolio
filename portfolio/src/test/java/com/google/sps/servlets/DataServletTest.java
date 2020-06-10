1cebfb316cf137ea7fe1cab6ccd688e8ff2d5ce1
Shaina Chen <shaina.sierra@gmail.com>
2020-06-10 16:55:48 +0000

    Add tests for edge case situations

--- portfolio/src/test/java/com/google/sps/servlets/DataServletTest.java
+++ portfolio/src/test/java/com/google/sps/servlets/DataServletTest.java
@@ -3,6 +3,12 @@ package com.google.sps.servlets;
 import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
+import static com.google.sps.Constants.COMMENT_ENTITY_NAME;
+import static com.google.sps.Constants.COMMENT_NAME_ID;
+import static com.google.sps.Constants.COMMENT_TEXT_ID;
+import static com.google.sps.Constants.REQUEST_NAME_PARAM;
+import static com.google.sps.Constants.REQUEST_COMMENT_PARAM;
+import static com.google.sps.Constants.REQUEST_NUMCOMMENTS_PARAM;
 
 import com.google.sps.CommentEntity;
 import com.google.sps.Comments;
@@ -37,6 +43,7 @@ public class DataServletTest {
 
   private DataServlet dataServlet;
   private StringWriter stringWriter;
+  private DatastoreService ds;
   private final LocalServiceTestHelper datastoreConfiguration =
       new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
 
@@ -47,6 +54,7 @@ public class DataServletTest {
     dataServlet.init();
     stringWriter = new StringWriter();
     datastoreConfiguration.setUp();
+    ds = DatastoreServiceFactory.getDatastoreService();
   }
 
   @After
@@ -56,10 +64,8 @@ public class DataServletTest {
 
   @Test
   public void testDataServlet_doGet_returnsSingleComment() throws Exception {
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-    CommentEntity comment = new CommentEntity("Bob", "Nice");
-    ds.put(comment.getEntity());
-    when(request.getParameter("numberofcomments")).thenReturn("5");
+    ds.put(new CommentEntity("Bob", "Nice").getEntity());
+    when(request.getParameter(REQUEST_NUMCOMMENTS_PARAM)).thenReturn("5");
     when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
     
     dataServlet.doGet(request, response);
@@ -69,7 +75,7 @@ public class DataServletTest {
 
   @Test
   public void testDataServlet_doGet_returnsNoComment() throws Exception {
-    when(request.getParameter("numberofcomments")).thenReturn("10");
+    when(request.getParameter(REQUEST_NUMCOMMENTS_PARAM)).thenReturn("10");
     when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
     
     dataServlet.doGet(request, response);
@@ -77,12 +83,11 @@ public class DataServletTest {
     assertEquals(stringWriter.getBuffer().toString().trim(), "[]");
   }
 
+  /* Default max number of comments is 10, test expected to return up to 10 comments in datastore */
   @Test
-  public void testDataServlet_doGet_returnsCommentByUsingDefaultCommentNumberWithIllegalParameterSpecification() throws Exception {
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-    CommentEntity comment = new CommentEntity("Bob", "Nice");
-    ds.put(comment.getEntity());
-    when(request.getParameter("numberofcomments")).thenReturn("0");
+  public void testDataServlet_doGet_returnsMaxNumCommentsWithIllegalNumComments() throws Exception {
+    ds.put(new CommentEntity("Bob", "Nice").getEntity());
+    when(request.getParameter(REQUEST_NUMCOMMENTS_PARAM)).thenReturn("-1");
     when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
     
     dataServlet.doGet(request, response);
@@ -92,12 +97,9 @@ public class DataServletTest {
 
   @Test
   public void testDataServlet_doGet_returnsMultipleComments() throws Exception {
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-    CommentEntity comment1 = new CommentEntity("Bob", "Nice");
-    CommentEntity comment2 = new CommentEntity("Sally", "This is a test comment");
-    ds.put(comment1.getEntity());
-    ds.put(comment2.getEntity());
-    when(request.getParameter("numberofcomments")).thenReturn("5");
+    ds.put(new CommentEntity("Bob", "Nice").getEntity());
+    ds.put(new CommentEntity("Sally", "This is a test comment").getEntity());
+    when(request.getParameter(REQUEST_NUMCOMMENTS_PARAM)).thenReturn("5");
     when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
     
     dataServlet.doGet(request, response);
@@ -105,17 +107,54 @@ public class DataServletTest {
     assertEquals(stringWriter.getBuffer().toString().trim(), "[\"Bob: Nice\",\"Sally: This is a test comment\"]");
   }
 
+  /* Default max number of comments is 10, test expected to return 10 comments */
+  @Test
+  public void testDataServlet_doGet_postsMaxNumCommentsWithMoreThanMaxCommentsGivenAndRequested() throws Exception {
+    for (int i=0; i< 12; i++) {
+      ds.put(new CommentEntity("Bob", "Nice").getEntity());
+    }
+    when(request.getParameter(REQUEST_NUMCOMMENTS_PARAM)).thenReturn("12");
+    when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+    
+    dataServlet.doGet(request, response);
+
+    assertEquals(stringWriter.getBuffer().toString().trim(), 
+    "[\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\",\"Bob: Nice\"]");
+  }
+
   @Test
   public void testDataServlet_doPost_postsSingleComment() throws Exception {
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-    when(request.getParameter("name-input")).thenReturn("Alice");
-    when(request.getParameter("comment-input")).thenReturn("My comment");
+    when(request.getParameter(REQUEST_NAME_PARAM)).thenReturn("Alice");
+    when(request.getParameter(REQUEST_COMMENT_PARAM)).thenReturn("My comment");
 
     dataServlet.doPost(request, response);
     
-    List<Entity> results = ds.prepare(new Query("Comment")).asList(FetchOptions.Builder.withDefaults());
+    List<Entity> results = ds.prepare(new Query(COMMENT_ENTITY_NAME)).asList(FetchOptions.Builder.withDefaults());
     assertEquals(1, results.size());
-    assertEquals("Alice", results.get(0).getProperty("nameText"));
-    assertEquals("My comment", results.get(0).getProperty("commentText"));
+    assertEquals("Alice", results.get(0).getProperty(COMMENT_NAME_ID));
+    assertEquals("My comment", results.get(0).getProperty(COMMENT_TEXT_ID));
+  }
+
+  @Test
+  public void testDataServlet_doPost_postsAnonymousSingleComment() throws Exception {
+    when(request.getParameter(REQUEST_NAME_PARAM)).thenReturn("");
+    when(request.getParameter(REQUEST_COMMENT_PARAM)).thenReturn("My comment");
+
+    dataServlet.doPost(request, response);
+    
+    List<Entity> results = ds.prepare(new Query(COMMENT_ENTITY_NAME)).asList(FetchOptions.Builder.withDefaults());
+    assertEquals(1, results.size());
+    assertEquals("", results.get(0).getProperty(COMMENT_NAME_ID));
+    assertEquals("My comment", results.get(0).getProperty(COMMENT_TEXT_ID));
+  }
+
+  @Test
+  public void testDataServlet_doPost_postsNothingGivenNoComment() throws Exception {
+    when(request.getParameter(REQUEST_NAME_PARAM)).thenReturn("Alice");
+
+    dataServlet.doPost(request, response);
+    
+    List<Entity> results = ds.prepare(new Query(COMMENT_ENTITY_NAME)).asList(FetchOptions.Builder.withDefaults());
+    assertEquals(0, results.size());
   }
 }
