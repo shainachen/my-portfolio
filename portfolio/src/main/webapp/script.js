c02b023b01179601f0830996889e2053b69650da
Shaina Chen <shaina.sierra@gmail.com>
2020-06-11 00:51:18 +0000

    Add Google Map into website with markers

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,8 +15,35 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
   const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+    document.getElementById('map'),
+    {center: {lat: 37.871853, lng: -122.258423}, zoom: 15});
+
+  addLandmark(map, /*lat=*/37.8703, /*lng=*/-122.2595, 
+    /*title=*/'Sather Gate', 
+    /*description=*/ '<img src=\'images/SatherGate.JPG\'><div>Sather Gate: Berkeley\'s green gate to campus</div>');
+
+  addLandmark(map, /*lat=*/37.8746, /*lng=*/-122.2496, 
+    /*title=*/'Best Views of Berkeley', 
+    /*description=*/ '<img src=\'images/BigC.JPG\'><div>This 10 minute hike gives you the best views of Berkeley\'s "C" and the SF bay... plus, a swing!</div>');
+  
+  addLandmark(map, /*lat=*/37.871944, /*lng=*/-122.257778, 
+    /*title=*/'Campanile', 
+    /*description=*/ '<img src=\'images/Campanile.JPG\'><div>Iconic bell tower of Berkeley. The rings signal the hours... so you\'ll never miss a final</div>');
+
+  addLandmark(map, /*lat=*/37.8786, /*lng=*/-122.2686, 
+    /*title=*/'Greatest potato puffs you\'ll ever eat', 
+    /*description=*/'<img src=\'images/PotatoPuff.JPG\'><div>Find the best potato puffs and a menu that changes with the seasons here.</div>');
+}
+
+/** Adds a marker that shows an info window when clicked. */
+function addLandmark(map, lat, lng, title, description) {
+  const marker = new google.maps.Marker(
+      {position: {lat: lat, lng: lng}, map: map, title: title});
+
+  const infoWindow = new google.maps.InfoWindow({content: description});
+  marker.addListener('click', () => {
+    infoWindow.open(map, marker);
+  });
 }
 
 /**
