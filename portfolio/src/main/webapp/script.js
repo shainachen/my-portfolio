17331a24658cba6aeeed827b24696e234544efef
Shaina Chen <shaina.sierra@gmail.com>
2020-06-11 00:50:39 +0000

    Add images for map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 /**
  * Adds a random fact to the page.
  */
