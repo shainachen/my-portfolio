f3e6b68acbb8ab3fb8c089ad15c99763fb56a612
Shaina Chen <shaina.sierra@gmail.com>
2020-06-15 20:17:11 +0000

    Switch class to AutoValue class

--- portfolio/src/main/java/com/google/sps/CommentEntity.java
+++ portfolio/src/main/java/com/google/sps/CommentEntity.java
@@ -1,22 +1,22 @@
 package com.google.sps;
-
+ 
 import static com.google.sps.Constants.COMMENT_ENTITY_NAME;
 import static com.google.sps.Constants.COMMENT_NAME_ID;
 import static com.google.sps.Constants.COMMENT_TEXT_ID;
-
+ 
 import com.google.appengine.api.datastore.Entity;
 import com.google.auto.value.AutoValue;
-
+ 
 @AutoValue
 /* A comment made by a user */
 abstract class CommentEntity {
   static CommentEntity create(String name, String commentText) {
     return new AutoValue_CommentEntity(name, commentText);
   }
-
+ 
   abstract String name();
   abstract String commentText();
-
+ 
   public Entity toEntity() {
     Entity entity = new Entity(COMMENT_ENTITY_NAME);
     entity.setProperty(COMMENT_NAME_ID, name());
