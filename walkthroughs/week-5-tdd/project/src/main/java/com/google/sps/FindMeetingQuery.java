ac83ef8eb2618ff506fb6d711045772f4459637b
Shaina Chen <shaina.sierra@gmail.com>
2020-06-17 00:42:26 +0000

    Implement querying for meeting times

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,48 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> meetingAttendees = request.getAttendees();
+    long meetingDuration = request.getDuration();
+    Collection<TimeRange> availableTimes = new ArrayList();
+
+    if (meetingDuration > TimeRange.WHOLE_DAY.duration()) {
+      return availableTimes;
+    }
+    
+    if (events.isEmpty() || meetingAttendees.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    List eventsList = new ArrayList(events);
+    Collections.sort(eventsList, Event.ORDER_BY_START);
+    
+    int start = TimeRange.START_OF_DAY;
+    for (Event event : events){
+      if (!Collections.disjoint(event.getAttendees(), meetingAttendees)) {
+        int eventStart = event.getWhen().start();
+        int eventEnd = event.getWhen().end();
+
+        if (start + meetingDuration <= eventStart) {
+          availableTimes.add(TimeRange.fromStartEnd(start, eventStart, false));
+        }
+
+        if (eventEnd > start) {
+          start = eventEnd;
+        }
+      }
+    }
+
+    if (meetingDuration + start <= TimeRange.END_OF_DAY) {
+      availableTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+    }
+    return availableTimes;
   }
 }
